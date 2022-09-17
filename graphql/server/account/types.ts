4c962ea1d9c2ed8ef28af16264e3930a6c33506b
Sebastián Gómez <sgr00114@gmail.com>
2022-09-17 11:16:46 -0500

    FEAT: Add account types

---
+++ graphql/server/account/types.ts
@@ -0,0 +1,38 @@
+import { gql } from 'apollo-server-micro';
+
+const AccountTypes = gql`
+  type Account {
+    id: ID
+    email: String
+    password: String
+    link: String
+    user: User
+  }
+
+  input AccountCreateInput {
+    email: String!
+    password: String!
+    link: String!
+    user_id: String!
+  }
+
+  input AccountUpdateInput {
+    email: String
+    password: String
+    link: String
+    user_id: String
+  }
+
+  type Query {
+    getAccount(id: String): Account
+    getAccounts: [Account]
+  }
+
+  type Mutation {
+    createAccount(data: AccountCreateInput): Account
+    updateAccount(id: String, data: AccountUpdateInput): Account
+    deleteAccount(id: String): Account
+  }
+`;
+
+export { AccountTypes };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,11 +1,18 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
 import { UserTypes } from '@graphql/server/user/types';
+import { ArtworkTypes } from '@graphql/server/artwork/types';
+import { AccountTypes } from '@graphql/server/account/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
-const globalTypes: DocumentNode[] = [CommonTypes, UserTypes];
+const globalTypes: DocumentNode[] = [
+  CommonTypes,
+  UserTypes,
+  ArtworkTypes,
+  AccountTypes,
+];
 
 export { globalTypes };
