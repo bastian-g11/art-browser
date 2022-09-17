ff39c2a50f50045e10b8bdc0c44f80f7ea33a264
Sebastián Gómez <sgr00114@gmail.com>
2022-09-16 22:06:55 -0500

    Project initialization

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import useApolloClient from 'hooks/useApolloClient';
+import { ApolloProvider } from '@apollo/client';
+import 'react-toastify/dist/ReactToastify.css';
+import 'styles/globals.css';
+
+const MyApp = ({ Component, pageProps: { ...pageProps } }: AppProps) => {
+  const { client } = useApolloClient();
+  return (
+    <>
+      <Head>
+        <title>Page Name</title>
+        <meta name='description' content='content' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
+};
+
+export default MyApp;
---
+++ types/index.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest } from 'next/types';
+
+export interface Context {
+  req: NextApiRequest;
+}
+
+interface ResolverFunction {
+  [key: string]: (parent: any, args: any, context: Context, info: any) => any;
+}
+
+export interface Resolver {
+  Query: ResolverFunction;
+  Mutation: ResolverFunction;
+  [key: string]: ResolverFunction;
+}
+
+export type MockModel = {
+  id: string;
+  name: string;
+  description: string;
+};
