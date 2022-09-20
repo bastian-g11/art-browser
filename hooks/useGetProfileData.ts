89ba6591aa4f36b818f6e172e4f3bb2900ab9a3f
Sebastián Gómez <sgr00114@gmail.com>
2022-09-20 12:30:19 -0500

    FEAT: Artworks are displayed by using data from User Context

--- hooks/useGetProfileData.ts
+++ hooks/useGetProfileData.ts
@@ -1,39 +1,21 @@
 import { useQuery } from '@apollo/client';
 import { GET_USER_DATA } from '@graphql/client/queries/users';
 import { useUserContext } from 'providers/UserProvider';
-import { useState, useEffect } from 'react';
 
 const useGetProfileData = () => {
   const user = useUserContext();
-  const [artworks, setArtworks] = useState<Artwork[]>([]);
-
-  const { isLoading, refetch } = useQuery(GET_USER_DATA, {
+  const { data, loading } = useQuery(GET_USER_DATA, {
     variables: {
-      getUserId: '',
+      // FIXME: Should be User ID
+      getUserId: user.id,
     },
-    skip: true,
     fetchPolicy: 'cache-and-network',
   });
 
-  useEffect(() => {
-    const getData = async () => {
-      console.log('ID SENT', user.id);
-
-      const { data } = await refetch({ getUserId: user.id });
-      console.log(
-        '🚀 ~ file: useGetProfileData.ts ~ line 21 ~ getData ~ data',
-        data
-      );
-
-      // setArtworks(mappedArtworks);
-    };
-
-    getData();
-  }, [user]);
-
   return {
-    artworks,
-    isLoading,
+    user,
+    artworks: data.getUser.artworks,
+    isLoading: loading,
   };
 };
 
